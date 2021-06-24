0b0d546c017740fa7a3473873dc0c99fc14dcfe0
Kyle Ryder <kryder1@umd.edu>
2021-06-24 09:17:48 -0400

    added SQL queries and some other functionalities

--- src/Driver.java
+++ src/Driver.java
@@ -1,15 +1,22 @@
-package src;
+
 
 import java.io.*;
 import java.util.*;
+import java.sql.*;
 
 public class Driver {
 
     public static List<Exercise> exercises = new ArrayList<Exercise>();
+
     public static void main(String[] args){
         Scanner s = new Scanner(System.in);
+        System.out.print("Username: ");
+        String user = s.nextLine();
+        System.out.print("Password: ");
+        String pass = s.nextLine();
         
-        exercises = Exercise.initList();
+        exercises = Exercise.startUp(user, pass);
+        System.out.println();
         
         //Gui.launchGui();
 
@@ -26,7 +33,7 @@ public class Driver {
 
             // Help
             else if (opt.equals("help")){
-                System.out.println("Commands:\tprint\tadd\texit");
+                System.out.println("Commands:\tprint\tadd\tdescribe\texit");
             }
 
             // Print existing exercises
@@ -48,22 +55,29 @@ public class Driver {
             else if (opt.equals("add")){
                 System.out.println("Please type an exercise to add: ");
                 String line = s.nextLine().toLowerCase();
+                System.out.println("Please enter a description: ");
+                String desc = s.nextLine();
+                Exercise e = new Exercise(line, desc);
 
-                if (Exercise.containsExercise(new Exercise(line))){
+                if (Exercise.containsExercise(e)){
                     System.out.println("Exercise already exists.");
                 }
                 else {
-                    exercises.add(new Exercise(line));
-
-                    // Adds to file
-                    try (BufferedWriter output = new BufferedWriter(new FileWriter("files/exercises.txt", true))){
-                    	output.write("\n" + line);
-                    }
-                    catch (Exception e){
-                        System.out.println(e);
-                        System.exit(1);
-                    }
+                    exercises.add(e);
+                    Exercise.addQuery(e);
                 }
+            }
+            else if (opt.equals("describe")) {
+            	System.out.println("Please type an exercise name: ");
+            	String line = s.nextLine().toLowerCase();
+            	
+            	Exercise e = Exercise.getExercise(line);
+            	if (e == null) {
+            		System.out.println("Exercise not found. Please type an existing exercise");
+            	}
+            	else {
+            		System.out.println(e.getDescription());
+            	}
             }
             else {
                 System.out.println("Invalid command. Please type 'help' for options.");
--- src/Exercise.java
+++ src/Exercise.java
@@ -1,53 +1,105 @@
-package src;
+
 
 import java.util.*;
-import java.io.*;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
+import java.sql.*;
 
 
 public class Exercise {
 
     private String name;
+    private String description;
+
     private static List<Exercise> exercises = new ArrayList<Exercise>();
+
+    private static Connection connect = null;
+    private static Statement statement = null;
+    private static PreparedStatement preparedStatement = null;
+    private static ResultSet resultSet = null;
 
 
     public Exercise(String name){
-        this.name = name.toUpperCase();
+        this.name = name;
+        this.description = "";
+    }
+
+    public Exercise(String name, String description){
+        this.name = name;
+        this.description = description;
     }
 
     public String getName(){
         return name;
     }
 
+    public String getDescription(){
+        return description;
+    }
+
     public static void addExercise(Exercise e){
         exercises.add(e);
+    }
+    
+    public static Exercise getExercise(String name) {
+    	for (Exercise e : exercises) {
+    		if (e.getName().equals(name)) {
+    			return e;
+    		}
+    	}
+    	return null;
     }
 
     public static List<Exercise> exerciseList(){
         return exercises;
     }
     
-    public static List<Exercise> initList() {
-        File file = new File("files/exercises.txt");
+    public static List<Exercise> startUp(String user, String pass) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connect = DriverManager.getConnection("jdbc:mysql://localhost/exercises", user, pass);
 
-        // read in existing exercises from file
-        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                exercises.add(new Exercise(line.toUpperCase()));
+            statement = connect.createStatement();
+
+            resultSet = statement.executeQuery("select name, description from exercises");
+
+
+            while (resultSet.next()) {
+                String exName = resultSet.getString("name");
+                String exDesc = resultSet.getString("description");
+                exercises.add(new Exercise(exName, exDesc));
+                
             }
         }
-        catch (Exception e){
-            System.out.println(e);
+        catch (SQLException e){
+            e.printStackTrace();
             System.exit(1);
         }
+        catch (ClassNotFoundException c) {
+        	c.printStackTrace();
+        	System.exit(2);
+        }
+
+        
 
         return exercises;
     }
 
+    public static boolean addQuery(Exercise e) {
+    	String name = e.getName();
+    	String desc = e.getDescription();
+    	try {
+    		preparedStatement = connect.prepareStatement("INSERT INTO exercises VALUES (?,?)");
+    		preparedStatement.setString(1, name);
+    		preparedStatement.setString(2, desc);
+    		preparedStatement.execute();
+    	}
+    	catch (SQLException s) {
+    		s.printStackTrace();
+    		return false;
+    	}
+    	
+    	return true;
+    }
+    
     public static boolean containsExercise(Exercise e) {
         for (Exercise i : exercises){
             if (i.getName().equals(e.getName())){
